ef08eac4cd0495e80d25051825791b2d7c1b10a5
DESKTOP-P4FL1OC\Rasika <amilajayamaha96@gmail.com>
2023-07-04 11:09:12 +0530

    simple http server in node js

--- my-first-node-code.js
+++ my-first-node-code.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    res.end('Hello World');
+});
+
+server.listen(8000, 'localhost', () => {
+    console.log('NodeJS is now listening on localhost:8000');
+});
