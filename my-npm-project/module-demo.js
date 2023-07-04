a5870d445f26d84519ecead7aa2fef8d872f2ce2
DESKTOP-P4FL1OC\Rasika <amilajayamaha96@gmail.com>
2023-07-04 11:32:09 +0530

    ascii text generator

---
+++ my-npm-project/module-demo.js
@@ -0,0 +1,6 @@
+let ascii_text_generator = require('ascii-text-generator');
+
+let input_text = "Node Express Beginner";
+let ascii_text =ascii_text_generator(input_text,"3");
+ 
+console.log(ascii_text);
