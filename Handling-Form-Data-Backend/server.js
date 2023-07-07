9a601e7021fc5ff2091c84963123b070067c2a60
DESKTOP-P4FL1OC\Rasika <amilajayamaha96@gmail.com>
2023-07-07 11:59:27 +0530

    handling form data

---
+++ Handling-Form-Data-Backend/server.js
@@ -0,0 +1,80 @@
+var express = require("express");
+var db = require("./database.js");
+var app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+
+
+ const cors = require("cors");
+ app.use(
+  cors({
+    exposedHeaders:["Content-Length", "X-Foo", "X-Bar"],
+    credentials: true,
+    origin:"*",
+  })
+ );
+
+// Start server
+var HTTP_PORT = 8000;
+
+app.listen(HTTP_PORT, () => {
+  console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
+});
+
+//HTTP GET method
+app.get("/api/products", (req, res, next) => {
+  try{
+    var sql = "select * from products";
+    var params = [];
+
+    db.all(sql, params, (err, rows) => {
+      if (err){
+        res.status(400).json({ error: err.message});
+        return;
+      }
+
+      res.status(200).json({
+        message: "success",
+        data: rows,
+      });
+    });
+
+  }catch(E){
+    res.status(400).send(E);
+
+  }
+});
+
+//HTTP POST method
+app.post("/api/products", (req, res, next) => {
+  try{
+   const{
+    productName,
+    description,
+    unitPrice
+   } = req.body;
+
+      var sql = 
+      "INSERT INTO products (productName, description, unitPrice) VALUES (?,?,?)";
+      var params = [productName, description, unitPrice];
+
+
+      db.run(sql, params, function (err, result) {
+        if(err){
+          res.status(400).json({ error: err.message});
+          return;
+        } else{
+
+          res.status(200).json({
+            message: "success",
+            data: req.body,
+          });
+        }
+      });
+    } catch (E){
+      res.status(400).send(E);
+    }
+  });
