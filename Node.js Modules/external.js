aeb6fec35f251b67f6a6ece6cbd4915f61b3e2ba
DESKTOP-P4FL1OC\Rasika <amilajayamaha96@gmail.com>
2023-07-17 11:08:46 +0530

    local modules

---
+++ Node.js Modules/external.js
@@ -0,0 +1,14 @@
+var details = {
+    levell: function (level1) {
+        console.log('level1:' + level1);
+    },
+
+    level2: function (level2) {
+        console.log('level2:' + level2);
+    },
+    level3: function (level3) {
+        console.log('level3:' + level3);
+    },
+}
+
+module.exports = details;
