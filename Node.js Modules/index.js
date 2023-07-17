50906c2b34a5c42b8f1484c3019c19fcb7ee53e0
AmilaJayamaha <136047885+AmilaJayamaha@users.noreply.github.com>
2023-07-17 11:15:52 +0530

    local modules

--- Node.js Modules/index.js
+++ Node.js Modules/index.js
@@ -1,6 +1,2 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-
-});
-
-server.listen(5000);
+var myLogModule = require('./external.js');
+myLogModule.level1(' Local Module Example');
